752ac8f23b5b874a208c83e6226c49777c4d1762
byoon97 <byoon5397@gmail.com>
2022-11-09 13:13:41 -0500

    header and banner

---
+++ components/Banner.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Banner() {
+  return (
+    <div className="flex justify-between items-center bg-yellow-400 border-y border-black py-10 lg:py-0">
+      <div className="px-10 space-y-5">
+        <h1 className="text-6xl max-w-xl font-serif">
+          <span className="underline decoration-blac decoration-w-4">
+            Motive
+          </span>{" "}
+          is a place to give or recieve motivation
+        </h1>
+        <h2>
+          Simply write about any topic or any thoughts you are having and share
+          it to the world!
+        </h2>
+      </div>
+      <img
+        className="hidden:md-inline-flex h-32 lg:h-full"
+        src="Motive Logo.PNG"
+        alt=""
+      />
+    </div>
+  );
+}
+
+export default Banner;
